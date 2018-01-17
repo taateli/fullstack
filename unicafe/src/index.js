0439072739b74955a38f5f2ea18bf28784fa95aa
Tatu Helander <tatu.helander@helsinki.fi>
2018-01-17 15:28:01 +0200

    Pakolliset done

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -3,83 +3,134 @@ import ReactDOM from 'react-dom'
 
 class App extends React.Component {
     constructor() {
-      super()
-      this.state = {
-        hyva: 0,
-        neutraali: 0,
-        huono: 0,
-        keskiarvo: 0,
-        lukumaara: 0
-      }
+        super()
+        this.state = {
+            hyva: 0,
+            neutraali: 0,
+            huono: 0,
+            keskiarvo: 0,
+            lukumaara: 0
+        }
     }
-    
+
 
     klikHyva = () => {
         this.setState({
-          hyva: this.state.hyva + 1,
-          keskiarvo: this.state.keskiarvo +1,
-          lukumaara: this.state.lukumaara +1  
-          //kaikki: this.state.kaikki.concat('v')
+            hyva: this.state.hyva + 1,
+            keskiarvo: this.state.keskiarvo + 1,
+            lukumaara: this.state.lukumaara + 1
+            //kaikki: this.state.kaikki.concat('v')
         })
         renderoi()
         console.log("hyvä")
-      }
+    }
 
 
     klikNeutraali = () => {
         this.setState({
-          neutraali: this.state.neutraali + 1,
-          lukumaara: this.state.lukumaara +1
-          //kaikki: this.state.kaikki.concat('v')
+            neutraali: this.state.neutraali + 1,
+            lukumaara: this.state.lukumaara + 1
+            //kaikki: this.state.kaikki.concat('v')
         })
         renderoi()
         console.log("neut")
-      }
+    }
 
 
     klikHuono = () => {
         this.setState({
-          huono: this.state.huono + 1,
-          keskiarvo: this.state.keskiarvo -1,
-          lukumaara: this.state.lukumaara +1
-          //kaikki: this.state.kaikki.concat('v')
+            huono: this.state.huono + 1,
+            keskiarvo: this.state.keskiarvo - 1,
+            lukumaara: this.state.lukumaara + 1
+            //kaikki: this.state.kaikki.concat('v')
         })
         renderoi()
         console.log("huono")
-      }
-    
-    render() {
-      return (
-        <div>
-            <h1>Anna palautetta</h1>
-          <button onClick={this.klikHyva}>
-            hyvä
-          </button>
-          <button onClick={this.klikNeutraali}>
-            neutraali
-          </button>
-          <button onClick={this.klikHuono}>
-            huono
-          </button>
-          <div>
-              <h1>Statistiikka</h1>
-              <p>hyvä: {this.state.hyva}</p>
-              <p>neutraali: {this.state.neutraali}</p>
-              <p>huono: {this.state.huono}</p>
-              <p>keskiarvo: {this.state.keskiarvo / this.state.lukumaara}</p>
-              <p>positiivisia: {((this.state.hyva / this.state.lukumaara) * 100).toFixed(2)} %</p>
-        </div>
-        </div>
-      )
     }
-  
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Anna palautetta</h1>
+                <Button
+                    handleClick={this.klikHyva}
+                    text="hyvä"
+                />
+                <Button
+                    handleClick={this.klikNeutraali}
+                    text="neutraali"
+                />
+                <Button
+                    handleClick={this.klikHuono}
+                    text="huono"
+                />
+                <Statistics
+                    state={this.state} 
+                />
+            </div>
+        )
+    }
+
 }
 
-const renderoi = () => {
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
 )
+
+const Statistics = (props) => {
+    if (props.state.lukumaara === 0) {
+        return(
+            <div>
+                <h1>Statistiikka</h1>
+                <em>yhtään palautetta ei ole annettu</em>
+            </div>
+        )
+    }
+    
+    return (
+        <div>
+        <h1>Statistiikka</h1>
+        <Statistic
+        text = "hyvä"
+        value = {props.state.hyva}
+        />
+        <Statistic
+        text = "neutraali"
+        value = {props.state.neutraali}
+        />
+        <Statistic
+        text = "huono"
+        value = {props.state.huono}
+        />
+        <Statistic
+        text = "keskiarvo"
+        value = {props.state.keskiarvo / props.state.lukumaara}
+        />
+        <Statistic
+        text = "positiivisia"
+        value = {((props.state.hyva / props.state.lukumaara) * 100).toFixed(2) + "%"}
+        />
+    </div>
+
+    )
+}
+
+const Statistic = ({text, value}) => {
+    return(
+        <p>{text}: {value}</p>
+    )
+}
+
+
+const renderoi = () => {
+    ReactDOM.render(
+        <App />,
+        document.getElementById('root')
+    )
 }
 
 renderoi()
